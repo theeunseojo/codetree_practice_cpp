f2e0ec8fff0004c775509931e97a7e649f8097d6
eunseo <ces803@naver.com>
2023-07-15 15:53:42 +0900

    2023.07.15 다중반복문 포맷 출력 / 19단 출력

---
+++ 6_14_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for ( int i = 1; i <= 19; i++){
+        for ( int j = 1; j <= 19; j++){
+            if ( j % 2 == 0) cout << i << " * " << j << " = " << i * j << endl;
+            else {
+                if( j == 19) cout << i << " * " << j << " = " << i * j << endl;
+                else cout << i << " * " << j << " = " << i * j << " / ";
+            }
+            
+        }
+    }
+    return 0;
+}
