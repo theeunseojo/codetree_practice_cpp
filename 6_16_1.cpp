a7567f08dfcbc06828d97146a2e053e86c751db4
eunseo <ces803@naver.com>
2023-07-18 00:18:57 +0900

    2023.07.18 n번 반복 / ab곱 n번 구하기

---
+++ 6_16_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    for ( int i = 0; i < n; i++){
+        int a , b;
+        cin >> a >> b;
+        int sum  = 1;
+        for ( int j = a; j <= b; j++ ){
+            sum *= j;
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
