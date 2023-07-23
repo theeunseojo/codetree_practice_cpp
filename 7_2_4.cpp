6ac6929bc5227e4ee641596c62f514630847fe8a
eunseo <ces803@naver.com>
2023-07-23 20:27:39 +0900

    2023.07.23 인덱스 참조 / 특정 위치 문자 2

---
+++ 7_2_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[10];
+    for(int i =0; i < 10; i++){
+        cin >> arr[i];
+    }
+    cout << arr[1] << " ";
+    cout << arr[4] << " ";
+    cout << arr[7] << " ";
+    return 0;
+}
