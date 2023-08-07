7277fd1261dbab67eb1b9409929f915918f8ca43
theeunseojo <ces80372@naver.com>
2023-08-07 21:42:48 +0900

    2023.08.07  2차원 배열 패턴 / 아래로 사각형

---
+++ 7_9_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int arr[10][10];
+
+    for(int i = 0; i < n; i++){
+        int num = i + 1;
+        for(int j = 0; j < n; j++){
+            arr[i][j] = num;
+            cout << arr[i][j] << " ";
+            num += n;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
