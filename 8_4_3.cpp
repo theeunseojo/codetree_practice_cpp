17710cc8191a5700c925916050a7c84627e98ca6
theeunseojo <ces80372@naver.com>
2023-08-22 21:47:46 +0900

    2023.08.22 이차원 배열 / 격자로 사각형

---
+++ 8_4_3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int arr[10][10];
+
+    //첫번째 행 채우기 
+    for(int j = 0; j < n; j++){
+        arr[0][j] = 1;
+    }
+
+    //첫번째 열 채우기 
+    for(int i = 0; i < n; i++){
+        arr[i][0] = 1;
+    }
+
+    for(int i = 1; i < n; i++){
+        for(int j = 1; j < n; j++){
+            arr[i][j] = arr[i - 1][j] + arr[i][j - 1] + arr[i - 1][j - 1];
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
