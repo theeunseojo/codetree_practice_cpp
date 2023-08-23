451d5d7b52d84e0dde6f6ab8cd3911f37b59f328
theeunseojo <ces80372@naver.com>
2023-08-23 22:19:03 +0900

    2023.08.23 공백 업슨 문자열 입력 출력 / 문자열 두번 출력

---
+++ 9_1_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+
+    cout << str << endl;
+    cout << str;
+    return 0;
+}
