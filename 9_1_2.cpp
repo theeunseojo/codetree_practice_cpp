7f095890104b5b7ed11b6f6da7fbd333bdf79f12
theeunseojo <ces80372@naver.com>
2023-08-25 22:56:05 +0900

    2023.08.25 공백없는 문자열 입력 출력 / 문자열 길이 출력

---
+++ 9_1_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str1;
+    string str2;
+    cin >> str1;
+    cin >> str2;
+    cout << str2.length() + str1.length();
+
+
+    return 0;
+}
