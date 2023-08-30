32171671cf20885d79c0e63f9345261143e032cd
theeunseojo <ces80372@naver.com>
2023-08-30 13:47:59 +0900

    2023.08.30 문자열 리스트 관리 / 단어 구분 3

---
+++ 9_3_7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string arr[10];
+
+    for(int i = 0; i < 10; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < 10; i++){
+        cout << arr[9 - i] << endl;
+    }
+    return 0;
+}
